fd44caab8f9949bec042c177462e7e77efbd4fe1
Vidya <vidyajejurkar@gmail.com>
2018-03-31 18:40:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vidya/paytmintegration/Constants.java
@@ -0,0 +1,15 @@
+package com.example.vidya.paytmintegration;
+
+/**
+ * Created by Vidya on 3/31/2018.
+ */
+
+public class Constants {
+    public static final String M_ID = "Harman81055207490308"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
